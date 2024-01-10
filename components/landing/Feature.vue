93e8f6df7c237528e10197abf3487d89a939bfff
Jérémy Riverain <jeremy@geekco.fr>
2024-01-10 07:04:38 +0100

    ♻️ refactor Features component

---
+++ components/landing/Feature.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex gap-4 items-start">
+    <div class="text-2xl">
+      {{ icon }}
+    </div>
+    <div>
+      <h2 class="font-semibold text-lg">{{ title }}</h2>
+      <p class="text-slate-500 mt-2 leading-relaxed">
+        {{ description }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  icon: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+</script>
--- components/landing/Features.vue
+++ components/landing/Features.vue
@@ -4,53 +4,49 @@ const features = [
     title: "3 modes de navigation",
     description:
       "3 modes de navigation pour découvrir le topo de différentes manières (en parcourant une liste, une carte des blocs, ou un index des blocs par commune et secteur).",
-    icon: "bx:bxs-bot",
+    icon: "🔥",
   },
   {
     title: "Carte intéractive",
     description:
       "Les blocs sont visibles sur une carte. En cliquant sur les marqueurs de position, on peut en voir le détail.",
-    icon: "bx:bxs-briefcase",
+    icon: "🌐",
   },
   {
     title: "Des infos détaillées",
     description:
       "L'application comporte des vues détaillées de chaque commune, secteur et bloc. Chaque bloc comporte une photo et la ligne de bloc est matérialisée par un trait rouge.",
-    icon: "bx:bxs-data",
+    icon: "🔖",
   },
   {
     title: "Trier et filtrer les blocs",
     description:
       "Des critères de recherche permettent de regrouper les blocs par commune, secteur, niveau de difficulté. On peut aussi afficher les blocs les plus récents.",
-    icon: "bx:bxs-window-alt",
+    icon: "🔍",
   },
   {
     title: "Accès hors-ligne (disponible prochainement)",
     description:
       "Téléchargez un secteur avant votre session et accédez ainsi au topo même quand la connexion internet est inexistante.",
-    icon: "bx:bxs-file-find",
+    icon: "📶",
   },
   {
     title: "Vous voulez contribuer au topo ?",
     description:
       "Vous faites régulièrement du bloc et vous aimeriez que certains blocs figurent sur le topo ? C'est possible. Pour cela, contactez-nous afin de devenir contributeur.",
-    icon: "bx:bxs-user",
+    icon: "🤝",
   },
 ];
 </script>
 
 <template>
   <div class="grid sm:grid-cols-2 md:grid-cols-3 mt-16 gap-16">
-    <div v-for="item of features" class="flex gap-4 items-start">
-      <div class="mt-1 bg-black rounded-full p-2 w-8 h-8 shrink-0">
-        <Icon class="text-white" :name="item.icon" />
-      </div>
-      <div>
-        <h2 class="font-semibold text-lg">{{ item.title }}</h2>
-        <p class="text-slate-500 mt-2 leading-relaxed">
-          {{ item.description }}
-        </p>
-      </div>
-    </div>
+    <LandingFeature
+      v-for="{ icon, title, description } of features"
+      :icon="icon"
+      :title="title"
+      :description="description"
+    >
+    </LandingFeature>
   </div>
 </template>
