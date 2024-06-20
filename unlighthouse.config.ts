6f06e95da01a226052f4bb9967ce9a128e3ea283
Jérémy Riverain <jeremy@geekco.fr>
2024-06-20 22:44:02 +0200

    🔧 update port for lighthouse script

--- unlighthouse.config.ts
+++ unlighthouse.config.ts
@@ -1,7 +1,7 @@
 const device = process.env.DEVICE ?? "mobile";
 
 export default {
-  site: "http://localhost:4444",
+  site: "http://localhost:8764",
   urls: ["/", "/contribute"],
   outputPath: `.unlighthouse/${device}`,
   scanner: {
